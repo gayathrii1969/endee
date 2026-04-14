1247e8b2b1dc98912ae5d7c58e50ff85e3d0d391
gayathrii1969 <ggayu1969@gmail.com>
2026-04-14 14:55:14 +0530

    Update app.py

--- student-ai-search/app.py
+++ student-ai-search/app.py
@@ -1,1 +1,39 @@
+from sentence_transformers import SentenceTransformer
+import streamlit as st
+import numpy as np
 
+# Load model
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+# Load data
+with open("data.txt", "r") as f:
+    data = f.readlines()
+
+# Convert to vectors
+vectors = model.encode(data)
+
+# Search function
+def search(query):
+    query_vec = model.encode([query])[0]
+
+    best_match = ""
+    best_score = -1
+
+    for i in range(len(vectors)):
+        score = np.dot(query_vec, vectors[i])
+
+        if score > best_score:
+            best_score = score
+            best_match = data[i]
+
+    return best_match
+
+
+# Streamlit UI
+st.title("📚 AI Student Notes Search")
+
+query = st.text_input("Ask your question:")
+
+if query:
+    result = search(query)
+    st.success(result)
